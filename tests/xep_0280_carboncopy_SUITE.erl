1619d08a732f98e770edd17441bf62acf6add53c
Simon Zelazny <simon.zelazny@erlang-solutions.com>
2014-04-30 16:36:24 +0200

    Move regular routing test to properties

--- tests/xep_0280_carboncopy_SUITE.erl
+++ tests/xep_0280_carboncopy_SUITE.erl
@@ -11,13 +11,16 @@ groups() ->
     [{essential, [discovering_support,
                   enabling_carbons,
                   disabling_carbons,
-                  receiving_messages_to_bare_jid,
-                  avoiding_carbons
+                  avoiding_carbons,
+                  non_enabled_clients_dont_get_sent_carbons,
+                  non_enabled_clients_dont_get_received_carbons
                  ]},
     {properties, [run_properties]}].
 
 prop_names() ->
-    [p_forward_received_messages, p_forward_sent_messages].
+    [p_forward_received_chat_messages, 
+     p_forward_sent_chat_messages,
+     p_normal_routing_to_bare_jid].
 
 init_per_suite(Config) ->
     escalus:init_per_suite(Config).
@@ -35,6 +38,7 @@ end_per_group(_, Config) ->
 
 init_per_testcase(CaseName,Config) ->
     escalus:init_per_testcase(CaseName,Config).
+
 end_per_testcase(CaseName,Config) ->
     escalus:end_per_testcase(CaseName,Config).
 
@@ -53,9 +57,11 @@ mk_prop(PropName, Config) ->
     {PropName, apply(?MODULE, PropName, [Config])}.
 
 
+
 %%
 %%  CT tests {group, essential}
 %%
+
 discovering_support(Config) ->
     escalus:story(
       Config, [{alice, 1}],
@@ -75,21 +81,6 @@ disabling_carbons(Config) ->
                   fun(Alice) -> carbons_get_enabled(Alice),
                                 carbons_get_disabled(Alice) end).
 
-receiving_messages_to_bare_jid(Config) ->
-    escalus:story(
-      Config, [{alice, 2}, {bob, 1}],
-      fun(Alice1,Alice2,Bob) ->
-              carbons_get_enabled([Alice1,Alice2]),
-              escalus_client:send(
-                Bob, escalus_stanza:chat_to(escalus_client:short_jid(Alice1),
-                                            <<"Most like a gentleman">>)),
-              escalus:assert(
-                is_chat_message, [<<"Most like a gentleman">>],
-                escalus_client:wait_for_stanza(Alice1)),
-              escalus:assert(
-                is_chat_message, [<<"Most like a gentleman">>],
-                escalus_client:wait_for_stanza(Alice2))
-      end).
 
 avoiding_carbons(Config) ->
     escalus:story(
@@ -106,57 +97,101 @@ avoiding_carbons(Config) ->
               [] = escalus_client:peek_stanzas(Alice2)
       end).
 
-no_of_resources() -> random:uniform(8).
+non_enabled_clients_dont_get_sent_carbons(Config) ->
+    escalus:story(
+      Config, [{alice, 2}, {bob, 1}],
+      fun(Alice1, Alice2, Bob) ->
+              Msg = escalus_stanza:chat_to(Bob, <<"And pious action">>),
+              escalus_client:send(Alice1, Msg),
+              escalus:assert(
+                is_chat_message, [<<"And pious action">>],
+                escalus_client:wait_for_stanza(Bob)),
+              escalus_client:wait_for_stanzas(Alice2, 1),
+              [] = escalus_client:peek_stanzas(Alice2)
+      end).
+
+non_enabled_clients_dont_get_received_carbons(Config) ->
+    escalus:story(
+      Config, [{alice, 2}, {bob, 1}],
+      fun(Alice1, Alice2, Bob) ->
+              Msg = escalus_stanza:chat_to(Alice1, <<"And pious action">>),
+              escalus_client:send(Bob, Msg),
+              escalus:assert(
+                is_chat_message, [<<"And pious action">>],
+                escalus_client:wait_for_stanza(Alice1)),
+              escalus_client:wait_for_stanzas(Alice2, 1),
+              [] = escalus_client:peek_stanzas(Alice2)
+      end).
+
 
 %%
 %% Property generators
 %% TODO: Consider moving to separate lib (escalus_prop?)
 %%
 
-p_enable_carbons(Config) ->
-    ?FORALL(N, no_of_resources(),
-            true_story(Config, [{alice, N}], fun carbons_get_enabled/1)).
-
-p_forward_received_messages(Config) ->
-    ?FORALL(N, no_of_resources(),
+p_forward_received_chat_messages(Config) ->
+    ?FORALL({N,Msg}, {no_of_resources(), utterance()},
             true_story(Config, [{alice, 1}, {bob, N}],
-                       fun all_bobs_secondary_resources_get_forwards/1)).
+                       fun(Users) ->
+                               all_bobs_other_resources_get_received_carbons(Users,Msg)
+                       end)).
 
-p_forward_sent_messages(Config) ->
-    ?FORALL(N, no_of_resources(),
+p_forward_sent_chat_messages(Config) ->
+    ?FORALL({N,Msg}, {no_of_resources(),utterance()},
             true_story(Config, [{alice, 1}, {bob, N}],
-                       fun all_bobs_secondary_resources_get_carbons/1)).
+                       fun(Users) ->
+                               all_bobs_other_resources_get_sent_carbons(Users,Msg)
+                       end)).
+
+p_normal_routing_to_bare_jid(Config) ->
+    ?FORALL({N,Msg}, {no_of_resources(),utterance()},
+            true_story(Config, [{alice, 1}, {bob, N}],
+                       fun(Users) ->
+                               all_bobs_resources_get_message_to_bare_jid(Users,Msg)
+                       end)).
+
 
 %%
 %% Test scenarios w/assertions
 %%
 
-all_bobs_secondary_resources_get_forwards([Alice,Bob1|Bobs]) ->
+all_bobs_resources_get_message_to_bare_jid([Alice,Bob1|Bobs], Msg) ->
+    %% All connected resources receive messages sent
+    %% to the user's bare JID without carbon wrappers.
+
     carbons_get_enabled([Bob1|Bobs]),
-    escalus_client:send(Alice,
-                        escalus_stanza:chat_to(Bob1, <<"'Tis most true:">>)),
+    escalus_client:send(
+      Alice, escalus_stanza:chat_to(escalus_client:short_jid(Bob1), Msg)),
+    GotMsg = fun(BobsResource) ->
+                     escalus:assert(
+                       is_chat_message, 
+                       [Msg], 
+                       escalus_client:wait_for_stanza(BobsResource))
+             end,
+    lists:foreach(GotMsg, [Bob1|Bobs]).    
+
+all_bobs_other_resources_get_received_carbons([Alice,Bob1|Bobs], Msg) ->
+    carbons_get_enabled([Bob1|Bobs]),
+    escalus_client:send(Alice, escalus_stanza:chat_to(Bob1, Msg)),
     GotForward = fun(BobsResource) ->
                          escalus:assert(
                            is_forwarded_received_message,
                            [escalus_client:full_jid(Alice),
                             escalus_client:full_jid(Bob1),
-                            <<"'Tis most true:">>],
+                            Msg],
                            escalus_client:wait_for_stanza(BobsResource)) end,
     lists:foreach(GotForward, Bobs).
 
-all_bobs_secondary_resources_get_carbons([Alice,Bob1|Bobs]) ->
+all_bobs_other_resources_get_sent_carbons([Alice,Bob1|Bobs], Msg) ->
     carbons_get_enabled([Bob1|Bobs]),
-    escalus_client:send(
-      Bob1, escalus_stanza:chat_to(Alice, <<"O heavy burden!">>)),
-    escalus:assert(
-      is_chat_message, [<<"O heavy burden!">>],
-      escalus_client:wait_for_stanza(Alice)),
+    escalus_client:send(Bob1, escalus_stanza:chat_to(Alice, Msg)),
+    escalus:assert(is_chat_message, [Msg], escalus_client:wait_for_stanza(Alice)),
     GotCarbon = fun(BobsResource) ->
                         escalus:assert(
                           is_forwarded_sent_message,
                           [escalus_client:full_jid(Bob1),
                            escalus_client:full_jid(Alice),
-                           <<"O heavy burden!">>],
+                           Msg],
                           escalus_client:wait_for_stanza(BobsResource)) end,
     lists:foreach(GotCarbon, Bobs).
 
@@ -176,8 +211,6 @@ carbons_get_enabled(Client) ->
     escalus:assert(is_iq, [<<"result">>], Result).
 
 
-
-
 %%
 %% Internal helpers
 %%
@@ -187,3 +220,18 @@ true_story(Config, UserSpecs, TestFun) ->
     try   escalus:story(Config, UserSpecs, TestFun), true
     catch E -> {error, E}
     end.
+
+%% Number of resources per users
+no_of_resources() -> random:uniform(4).
+
+%% A sample chat message
+utterance() ->
+    proper_types:oneof(
+      [<<"Now, fair Hippolyta, our nuptial hour">>,
+       <<"Draws on apace; four happy days bring in">>,
+       <<"Another moon: but, O, methinks, how slow">>,
+       <<"This old moon wanes! she lingers my desires">>,
+       <<"Like to a step-dame or a dowager">>,
+       <<"Long withering out a young man revenue.">>]).
+
+
