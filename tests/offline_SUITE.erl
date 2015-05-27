9adcd0452c68f2c6ca2b7ea7942b97b30b03c498
Stefan Strigler <stefan.strigler@1und1.de>
2015-05-27 11:20:44 +0200

    add test for #435 - race condition in mod_offline

--- tests/offline_SUITE.erl
+++ tests/offline_SUITE.erl
@@ -18,7 +18,7 @@ all() ->
     [{group, mod_offline_tests}].
 
 all_tests() ->
-    [simple_message].
+    [simple_message, timed_racecondition].
 
 groups() ->
     [{mod_offline_tests, [sequence], all_tests()}].
@@ -68,6 +68,42 @@ simple_message(Config) ->
     escalus_new_assert:mix_match([is_presence,
                                   is_chat(<<"Hi, Offline!">>)],
                                  Stanzas),
+    escalus_cleaner:clean(Config).
+
+timed_racecondition(Config) ->
+    %% Alice sends a message to Bob, who is about to log in.
+    escalus:story(
+      Config, [{alice, 1}],
+      fun(Alice) ->
+          %% We vary this scenario over time in order to find the sweet spot.
+          lists:foreach(
+            fun(I) ->
+                    Spec = escalus_users:get_userspec(Config, bob),
+                    {ok, Bob} = escalus_client:start(Config, Spec, <<"dummy">>),
+                    escalus:send(Bob, escalus_stanza:presence(<<"available">>)),
+
+                    %% Wait a little (varying time) until sending the message.
+                    receive
+                    after I -> ok
+                    end,
+
+                    Message = escalus_stanza:chat_to(bob, <<"Hi, Offline!">>),
+                    escalus:send(Alice, Message),
+
+                    %% He receives his initial presence and the message
+                    Stanzas = escalus:wait_for_stanzas(Bob, 2),
+                    escalus_new_assert:mix_match([is_presence,
+                                                  is_chat(<<"Hi, Offline!">>)],
+                                                 Stanzas),
+
+                    escalus_client:stop(Bob)
+            end,
+            %% The incident happens somewhere bettween 70 and 80 msec
+            %% - at least on my machine (tm).
+            lists:flatten([lists:duplicate(10, El) || El <- lists:seq(70,80)])
+           )
+      end),
+
     escalus_cleaner:clean(Config).
 
 %%%===================================================================
